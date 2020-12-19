b2c6d7004ec723664c32cb7a7a3530e13306bbb9
AhmedFay <AhmedFay@users.noreply.github.com>
2020-12-19 12:32:13 +0200

    ViewTask Activity and some fixes

---
+++ app/src/main/java/com/lab/todolist/ViewTaskActivity.java
@@ -0,0 +1,15 @@
+package com.lab.todolist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_task);
+
+    }
+}
